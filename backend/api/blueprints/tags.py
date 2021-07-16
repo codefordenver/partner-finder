f051cbddad84e824216a883d9418e6afa44a09a0
Wes Galbraith <galbwe92@gmail.com>
2021-07-15 23:31:02 -0600

    add remaining crud endpoints for tags

--- backend/api/blueprints/tags.py
+++ backend/api/blueprints/tags.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, current_app
 
 from sqlalchemy import text
+from sqlalchemy.exc import IntegrityError
 
 from ..auth import auth
 from ..db import db
@@ -9,7 +10,7 @@ from ..db import db
 tags_bp = Blueprint("tags", __name__)
 
 
-@tags_bp.route('/tags', methods=["GET", "POST"])
+@tags_bp.route("/tags", methods=["GET", "POST"])
 @auth("user")
 def tags_collection_view():
     if request.method == "GET":
@@ -35,27 +36,120 @@ def _get_all_tags(request):
         response_body = []
         for row in res:
             response_body.append(dict(row))
-        return {
-            "tags": response_body
-        }, 200
+        return {"tags": response_body}, 200
 
 
 def _create_tag(request):
 
-    tag = request.get_json().get('tag')
+    tag = request.get_json().get("tag")
 
     # TODO: handle missing tag field in request
 
-    query = text("""
+    query = text(
+        """
         INSERT INTO tags (tag)
         VALUES (:tag)
         RETURNING *;
-    """)
+    """
+    )
 
-    with db.get_engine().begin() as connection:
-        row = connection.execute(
+    try:
+        with db.get_engine().begin() as connection:
+            row = connection.execute(query, tag=tag).first()
+    except IntegrityError as e:
+        current_app.logger.error(e)
+        return {"message": f"Could not create a new tag: {e}"}, 422
+
+    return dict(row), 200
+
+
+@tags_bp.route("/tags/<int:id>", methods=["GET", "PUT", "DELETE"])
+@auth("user")
+def tag_view(id):
+    if request.method == "GET":
+        return _get_tag_by_id(id)
+    elif request.method == "PUT":
+        return _modify_tag_with_id(id, request)
+    elif request.method == "DELETE":
+        return _delete_tag_with_id(id)
+    return {"message": "Unknown http method"}, 404
+
+
+def _get_tag_by_id(id):
+    query = text(
+        """
+        SELECT * FROM tags
+        where id = :id
+        """
+    )
+
+    with db.get_connection() as conn:
+        row = conn.execute(query, id=id).first()
+
+    if row is None:
+        return {
+            "params": {
+                "id": id,
+            },
+            "message": "Could not find a tag with the given id.",
+        }, 404
+
+    return dict(row), 200
+
+
+def _modify_tag_with_id(id, request):
+    tag = request.get_json().get("tag")
+
+    if tag is None:
+        return {"message": "Missing required field 'tag' in request body."}, 422
+
+    query = text(
+        """
+        UPDATE tags
+        SET tag = :tag
+        WHERE id = :id
+        RETURNING *;
+    """
+    )
+
+    with db.get_engine().begin() as conn:
+        row = conn.execute(
             query,
-            tag=tag
+            id=id,
+            tag=tag,
         ).first()
+
+        if row is None:
+            return {
+                "params": {
+                    "id": id,
+                },
+                "body": request.get_json(),
+                "message": "Could not find tag with the given id",
+            }, 404
+
+        return dict(row), 200
+
+
+def _delete_tag_with_id(id):
+
+    query = text(
+        """
+        DELETE FROM tags
+        WHERE id = :id
+        RETURNING *;
+        """
+    )
+
+    with db.get_engine().begin() as conn:
+        row = conn.execute(query, id=id).first()
+
+    if row is None:
+        return {
+            "params": {
+                "id": id,
+            },
+            "message": "Could not find tag with the given id.",
+        }, 404
 
     return dict(row)
