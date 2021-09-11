8f0dc8c586202526e3ac63ac9bc094a5f0f455d6
Wes Galbraith <galbwe92@gmail.com>
2021-09-11 03:50:02 -0600

    Configure dotenv

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,9 @@
+import dotenv from 'dotenv';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/App';
+
+dotenv.config();
 
 ReactDOM.render(<App />, document.getElementById('root'));
