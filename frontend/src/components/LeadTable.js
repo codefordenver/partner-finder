b0867a9208a98ab5410c83a0b315b2921ea735b7
Claire Fields <clairefields15@gmail.com>
2021-09-28 18:06:42 -0600

    Create fn to create lead rows and move leadrow render into new component

--- frontend/src/components/LeadTable.js
+++ frontend/src/components/LeadTable.js
@@ -11,37 +11,13 @@ import {
 import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
 import DeleteOutlineOutlinedIcon from '@material-ui/icons/DeleteOutlineOutlined';
 import { useStyles } from './Home';
+import { LeadRow } from './LeadRow';
 
 export const LeadTable = ({ leads }) => {
   const classes = useStyles();
 
   const makeLeadRows = () => {
-    return leads.map((lead) => (
-      <TableRow>
-        <TableCell>{lead['company_name']}</TableCell>
-        <TableCell>{lead['contact_name']}</TableCell>
-        <TableCell>{lead['website']}</TableCell>
-        <TableCell>
-          {lead['facebook'] ||
-            lead['linkedin'] ||
-            lead['twitter'] ||
-            lead['instagram']}
-        </TableCell>
-        <TableCell>{lead['assignee']}</TableCell>
-        {/* TODO: get tags */}
-        <TableCell></TableCell>
-        <TableCell>
-          <Box display="flex" flexDirection="row" justifyContent="center">
-            <Box className={classes.roundButton}>
-              <EditOutlinedIcon />
-            </Box>
-            <Box className={classes.roundButton}>
-              <DeleteOutlineOutlinedIcon />
-            </Box>
-          </Box>
-        </TableCell>
-      </TableRow>
-    ));
+    return leads.map((lead) => <LeadRow lead={lead} />);
   };
 
   return (
