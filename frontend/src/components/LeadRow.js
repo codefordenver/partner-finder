f9f183d36e77a2fa9ab5971631d8c5207dc0f303
Mohammed Safwan S <mdsafwan3102001@gmail.com>
2021-10-12 14:07:30 +0530

    Replaced social media links with their respective icons

--- frontend/src/components/LeadRow.js
+++ frontend/src/components/LeadRow.js
@@ -3,6 +3,10 @@ import { TableRow, TableCell, Box } from '@material-ui/core';
 import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
 import DeleteOutlineOutlinedIcon from '@material-ui/icons/DeleteOutlineOutlined';
 import { useStyles } from './Home';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 const SocialMediaLink = ({ lead }) => {
   const link =
@@ -14,7 +18,21 @@ const SocialMediaLink = ({ lead }) => {
 
   return (
     <a href={link} target="no_blank">
-      {link}
+      {
+      link && link.includes('facebook') ? 
+      <FacebookIcon/> :
+
+      link && link.includes('linkedin') ? 
+      <LinkedInIcon/> :
+
+      link && link.includes('twitter') ?
+      <TwitterIcon/> :
+
+      link && link.includes('instagram') ?
+      <InstagramIcon/> :
+      
+      link
+      }
     </a>
   );
 };
