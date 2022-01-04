531a77891ffb5cc5001631206613d6153c549e1f
Wes Galbraith <galbwe92@gmail.com>
2022-01-04 08:20:50 -0700

    Create infra directory

---
+++ infra/infra/infra_stack.py
@@ -0,0 +1,29 @@
+from aws_cdk import (
+    # Duration,
+    Stack,
+    # aws_sqs as sqs,
+)
+from constructs import Construct
+
+class InfraStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # The code that defines your stack goes here
+
+        # example resource
+        # queue = sqs.Queue(
+        #     self, "InfraQueue",
+        #     visibility_timeout=Duration.seconds(300),
+        # )
+
+        # create vpc
+
+        # create ec2 instance for k8s master node
+
+        # create ec2 instance for k8s worker node
+
+        # create rds instance
+
+
