78111e9d2f0e2781ec9ac5093c0beb85f3f9254e
Wes Galbraith <galbwe92@gmail.com>
2022-01-09 09:23:02 -0700

    Start working on cdk stack

--- infra/infra/infra_stack.py
+++ infra/infra/infra_stack.py
@@ -2,6 +2,7 @@ from aws_cdk import (
     # Duration,
     Stack,
     # aws_sqs as sqs,
+    aws_ec2 as ec2,
 )
 from constructs import Construct
 
@@ -9,21 +10,58 @@ class InfraStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        vpc = self._setup_vpc()
+        ec2_instance = self._setup_ec2_instance(vpc)
 
-        # The code that defines your stack goes here
+        # TODO: create rds instance
+        rds_instance = self._setup_rds_instance(vpc)
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "InfraQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+    def _setup_vpc(self):
 
-        # create vpc
+        # TODO: determine cidr block
+        vpc = ec2.Vpc(
+            self,
+            "VPC",
+            cidr="10.0.0.0/16"
+        )
 
-        # create ec2 instance for k8s master node
+        # TODO: configure public access
 
-        # create ec2 instance for k8s worker node
+        # Iterate the private subnets
+        selection = vpc.select_subnets(
+            subnet_type=ec2.SubnetType.PRIVATE_WITH_NAT
+        )
 
-        # create rds instance
+        for subnet in selection.subnets:
+            pass
 
+        return vpc
 
+    def _setup_ec2_instance(self, vpc):
+        # define instance type
+        instance_type = ec2.InstanceType("t3.micro")
+        # define machine image
+        machine_image = ec2.MachineImage.latest_amazon_linux()
+
+        # create ec2 instance
+        ec2_instance = ec2.Instance(self, "Instance",
+            vpc=vpc,
+            instance_type=instance_type,
+            machine_image=machine_image,
+
+            # ...
+
+            block_devices=[
+                ec2.BlockDevice(
+                    device_name="/dev/sda1",
+                    volume=ec2.BlockDeviceVolume.ebs(50)
+                ),
+                ec2.BlockDevice(
+                    device_name="/dev/sdm",
+                    volume=ec2.BlockDeviceVolume.ebs(100)
+                ),
+            ]
+        )
+
+    def _setup_rds_instance(self, vpc):
+        pass
--- infra/tests/unit/test_infra_stack.py
+++ infra/tests/unit/test_infra_stack.py
@@ -5,10 +5,10 @@ from infra.infra_stack import InfraStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in infra/infra_stack.py
-def test_sqs_queue_created():
-    app = core.App()
-    stack = InfraStack(app, "infra")
-    template = assertions.Template.from_stack(stack)
+# def test_sqs_queue_created():
+#     app = core.App()
+#     stack = InfraStack(app, "infra")
+#     template = assertions.Template.from_stack(stack)
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
 #         "VisibilityTimeout": 300
